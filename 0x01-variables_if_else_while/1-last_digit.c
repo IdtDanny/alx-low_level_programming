83e46375afe0acd9b2ad5f9f317161e9982e636b
IdtDanny <idtdanny@gmail.com>
2023-03-17 11:53:53 +0000

    Last Digit Tested using modulus

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,9 +16,9 @@ int main(void)
 	last_digit = n % 10;
 
 	/* your code goes there */
-	if (n > 5)
+	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	else if ((n < 6) && (n != 0))
+	else if ((last_digit < 6) && (last_digit != 0))
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	else
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
