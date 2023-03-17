e851ffd2716365d32229734920e0f62d36145be7
IdtDanny <idtdanny@gmail.com>
2023-03-17 12:40:15 +0000

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+  * main - prints alphabets in lower case
+  * Return: 0
+  */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if ((alpha == 'q') || (alpha == 'e'))
+		{
+		}
+		else
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
