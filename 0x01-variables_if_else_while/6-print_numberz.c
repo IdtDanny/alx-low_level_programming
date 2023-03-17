5dacacccddd53c31bca23167f0e4cbfe0de40b6c
IdtDanny <idtdanny@gmail.com>
2023-03-17 18:22:23 +0000

    Reverse Alphabets

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+  * main - prints alphabets in lower case
+  * Return: 0
+  */
+int main(void)
+{
+	char n;
+
+	n='123456789';
+
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+  * main - prints alphabets in lower case
+  * Return: 0
+  */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
