3b326be7771f982ab16e59bef630d62400df15e2
IdtDanny <idtdanny@gmail.com>
2023-03-18 07:31:35 +0000

    Inventing Combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+  * main - prints alphabets in lower case
+  * Return: 0
+  */
+int main(void)
+{
+	int x;
+	int y;
+	int space;
+
+	space = 32; /* According to ASCII Table */
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if ((x != y) && (x < y))
+			{
+				putchar(x + 48);
+				putchar(y + 48);
+				if (x+y != 17)
+				{
+					putchar(',');
+					putchar(space);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
