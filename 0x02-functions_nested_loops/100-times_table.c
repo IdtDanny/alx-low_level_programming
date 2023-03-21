2e12b207233d54af3482585a8ba753a49045dfba
IdtDanny <idtdanny@gmail.com>
2023-03-21 18:09:36 +0000

    All Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
   * print_times_table - prints time of multiplication
-  *
+  * @n: parameter to be passed
   * Return: 0
   */
 void print_times_table(int n)
 {
 	int x, y, mul;
-	int i, k, j;
 
 	if (!(n < 0 || n > 15))
 	{
@@ -18,24 +17,19 @@ void print_times_table(int n)
 				mul = x * y;
 				if (mul > 99)
 				{
-					i = mul / 100;
-					j = (mul / 10) % 10;
-					k = mul % 10;
 					_putchar(',');
 					_putchar(32);
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(k + '0');
+					_putchar((mul / 100) + '0');
+					_putchar(((mul / 10) % 10) + '0');
+					_putchar((mul % 10) + '0');
 				}
 				else if (mul > 9)
 				{
-					i = mul % 10;
-					k = (mul - i) / 10;
 					_putchar(',');
 					_putchar(32);
 					_putchar(32);
-					_putchar(k + '0');
-					_putchar(i + '0');
+					_putchar(((mul - (mul % 10)) / 10) + '0');
+					_putchar((mul % 10) + '0');
 				}
 				else
 				{
