25a76f5893b640f04655ac5878b42e14b65d9a74
IdtDanny <idtdanny@gmail.com>
2023-03-23 14:03:26 +0000

    FizzMe

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,23 @@ int main(void)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (x % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (x % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
+		}
+		else if (x == 1)
+		{
+			printf("%d", x);
 		}
 		else
 		{
-			printf("%d ", x);
+			printf(" %d", x);
 		}
 	}
 	printf("\n");
