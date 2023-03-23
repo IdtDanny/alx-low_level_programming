4b7d649e4360ab948f0e4479789229980c256e6a
IdtDanny <idtdanny@gmail.com>
2023-03-23 12:33:59 +0000

    Print Lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_line - prints line instead of number
+  *
+  * Return: does not return anything
+  */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
+
