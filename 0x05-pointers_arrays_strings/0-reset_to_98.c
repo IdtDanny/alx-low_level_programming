ff0192f4234656616479cb152b582f33acd9a065
Idt Danny <idtdanny@gmail.com>
2023-04-08 21:42:22 +0000

    All corrected: Atoi function and Keygen

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-  * reset_to_98 - change value using pointer
-  *
-  * @n: pointer parameter
-  *
-  * Return: does not return anything
-  */
+ * reset_to_98 - updates the value to 98
+ *
+ * @n: parameter passed
+ *
+ * Return: nothing
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
