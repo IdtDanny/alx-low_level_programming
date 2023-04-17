6f9d603f54fe1c22ff1aa02eb3d55cb38788bfbf
IdtDanny <idtdanny@gmail.com>
2023-04-17 23:25:25 +0000

    New typedef struct dog_t added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,4 +20,16 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
  * Prints the structure content
  */
 void print_dog(struct dog *d);
+/**
+ * struct dog_t - new type dog_t of same function
+ * @name: dogs' name
+ * @age: dogs' age
+ * @owner: owner of the dog
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 #endif
