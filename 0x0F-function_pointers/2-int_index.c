aabe68fd863841f2a4bc147d2ff53a88fff827bb
Idt Danny <idtdanny@gmail.com>
2023-04-20 04:05:42 +0000

    INDEX done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "function_pointers.h"
 /**
  * int_index - return index place if comparison = true, else -1
@@ -12,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
-			return (-1);
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
