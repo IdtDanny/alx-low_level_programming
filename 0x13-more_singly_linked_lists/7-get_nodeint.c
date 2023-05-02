412c712cfc15515b5d6e74de336f9e33762e3cdd
IdtDanny <idtdanny@gmail.com>
2023-05-02 21:50:54 +0000

    Betty Corrected

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int x = 0;
 	listint_t *samp = head;
 
-	while (samp && x< index)
+	while (samp && x < index)
 	{
 		samp = samp->next;
 		x++;
